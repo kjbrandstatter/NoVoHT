5073ec20c457508fa3f105b32a1af6fbffeaec5e
Kevin <kjbrandstatter@gmail.com>
2012-09-25 23:40:42 -0500

    Added bashscript to automate benchmarking process
    Added parameter to csvout to facilitate identifying test type

---
+++ RunBench.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+logfile=runbench.log
+datafile=testresults.csv
+function persistent {
+echo "Persistent test"
+for i in {1,5}00000{,0,00,000}
+#for i in {1,5}00000 #{,0,00,000}
+do ./fbench $i fdata | tee -a $logfile | ./csvout.pl -out $datafile -type PERS
+done
+}
+function ssd_persistent {
+echo "Persistent test (SSD)"
+for i in {1,5}00000{,0,00,000}
+do ./fbench $i fdata | tee -a $logfile | ./csvout.pl -out $datafile -type SSD
+done
+}
+function hybrid_persistence {
+echo "Persistent test"
+for i in {1,5}00000{,0,00,000}
+do ./fbench $i fdata | tee -a $logfile | ./csvout.pl -out $datafile -type HYBRID
+done
+}
+function nopersistence {
+echo "Non persistent test"
+for i in {1,5}00000{,0,00,000}
+do ./fbench $i | tee -a $logfile | ./csvout.pl -out $datafile -type NOPERS
+done
+}
+function basic {
+persistent
+no_persistence
+}
+function extra {
+ssd_persistent
+hybrid_persistence
+}
+
+function all {
+basic
+extra
+}
+
+[[ $# -gt 0 ]] && $1 || echo Invalid Benchmark
