bb4c12193f7ceb90af0f9e355c894cf5d02bca80
Kevin <kjbrandstatter@gmail.com>
2013-05-05 16:56:54 -0500

    Added unit test for overwrite get string

--- tests/unit_tests.cpp
+++ tests/unit_tests.cpp
@@ -1,3 +1,9 @@
+#include <novoht.h>
+#include <iostream>
+typedef struct  {
+   bool success;
+   string message;
+} status;
 int test_write(){
    return -1;
 }
@@ -25,5 +31,30 @@ int test_writeFile(){
 int test_rewriteFile(){
    return -1;
 }
+
+status test_overwrite_get(){
+   NoVoHT *map = new NoVoHT("get.nht", 1000, -1);
+   status return_stat;
+   map->put("testkey", "testval");
+   string * meow = map->get("testkey");
+   if (meow->compare(string("testval")) != 0) {
+      return_stat.success = false;
+      return_stat.message = string("Get operation returned wrong value");
+      return return_stat;
+   }
+   *meow = string("newval");
+   meow = map->get("testkey");
+   if (meow->compare(string("testval")) != 0) {
+      return_stat.success = false;
+      return_stat.message = string("Get value overwritten");
+      return return_stat;
+   }
+   return_stat.success = true;
+   return_stat.message = string("Overwrite get Test Passed");
+   return return_stat;
+}
+
 int main(){
+   status test_stat = test_overwrite_get();
+   cout << test_stat.message << endl;
 }
